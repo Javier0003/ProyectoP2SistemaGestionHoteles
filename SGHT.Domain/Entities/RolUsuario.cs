721833af1e18e53fc8a238178d3bc25b686568f5
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-02-07 23:17:12 +0100

    update

--- SGHT.Domain/Entities/RolUsuario.cs
+++ SGHT.Domain/Entities/RolUsuario.cs
@@ -4,7 +4,7 @@ using SGHT.Domain.Base;
 
 namespace SGHT.Domain.Entities
 {
-    public class RolUsuario : Auditoria
+    public class RolUsuarios : Auditoria
     {
         public int IdRolUsuarios { get; set; }
     }
