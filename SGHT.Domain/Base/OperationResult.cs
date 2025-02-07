ea9b6c3f48e10f47e5ddcdd61831b8bb67273bdd
Jonaifry Rodriguez <jonaifryrodriguezdejesus5@gmail.com>
2025-02-06 22:53:30 -0400

    SGHT_DOMAIN_BASE

---
+++ SGHT.Domain/Base/OperationResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGHT.Domain.Base
+{
+    public class OperationResult
+    {
+        public bool? Success { get; set; }
+        public bool? Message { get; set; }
+        public dynamic? Data { get; set; }
+    }
+}
