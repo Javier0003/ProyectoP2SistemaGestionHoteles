128ca279aecee9ed863cdf45692d597723f49308
Javier0003 <jpere0500@gmail.com>
2025-02-10 15:11:14 -0400

    started persistance

---
+++ SGHT.Persistance/Context/SGHTContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGHT.Domain.Entities;
+
+namespace SGHT.Persistance.Context
+{
+    public class SGHTContext : DbContext
+    {
+        public SGHTContext(DbContextOptions<SGHTContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Catetgoria> catetgorias { get; set; }
+        public DbSet<Cliente> clientes { get; set; }
+        public DbSet<EstadoHabitacion> estadoHabitaciones { get; set; }
+        public DbSet<Habitacion> habitaciones { get; set; }
+        public DbSet<Piso> piso { get; set; }
+        public DbSet<Reservacion> reservaciones { get;set; }
+        public DbSet<RolUsuario> rolUsuarios { get; set; }
+        public DbSet<Servicios> servicios { get; set; }
+        public DbSet<Tarifas> tarifas { get; set; }
+        public DbSet<Usuarios> usuarios { get; set; }
+    }
+}
