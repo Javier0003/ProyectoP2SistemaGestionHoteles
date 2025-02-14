47cc96e8b45c39101367488872c0888a22b00fd1
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-02-14 01:00:05 +0100

    update

---
+++ SGHT.Persistance/Interfaces/IRecepcionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SGHT.Domain.Entities;
+using SGHT.Domain.Repositorio;
+
+namespace SGHT.Persistance.Interfaces
+{
+    public interface IRecepcionRepository : IBaseRepositorio<Recepcion>
+    {
+
+    }
+}
---
+++ SGHT.Persistance/Repositories/Ejemplo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGHT.Persistance.Repositories
+{
+    internal class Ejemplo
+    {
+    }
+}
