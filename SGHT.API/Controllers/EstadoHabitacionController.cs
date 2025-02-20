68432f6ff82f99350bd3667e82220a7f8d4d25dd
Edwin-Software-Developer <edwinperez200510@gmail.com>
2025-02-20 17:06:01 -0400

    Agregando el Create and Delate

--- SGHT.API/Controllers/EstadoHabitacionController.cs
+++ SGHT.API/Controllers/EstadoHabitacionController.cs
@@ -35,6 +35,38 @@ namespace SGHT.API.Controllers
             return Ok(Estado);
         }
 
+
+
+
+ [HttpPost("CrearEstadoHabitacion")]
+        public async Task<IActionResult> Post(EstadoHabitacion estadoHabitacion)
+        {
+            if (estadoHabitacion == null) return BadRequest("Peticion invalida");
+
+            var result = await _estadoHabitacionRepository.SaveEntityAsync(estadoHabitacion);
+
+            if (!result.Success) return BadRequest();
+            return Ok("Estado Asignado");
+        }
+
+
+        [HttpDelete("EliminarEstadoHabitacion/{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null) return BadRequest("Id is null");
+            bool isParsed = int.TryParse(id, out int result);
+            if (!isParsed) return BadRequest("Id must be a String");
+
+            var entity = await _estadoHabitacionRepository.GetEntityByIdAsync(result);
+            if (entity == null) return NotFound("Estado no encontrado");
+
+            var queryResult = await _estadoHabitacionRepository.DeleteEntityAsync(entity);
+            if (!queryResult.Success) return Problem();
+
+            return Ok("Estado eliminado");
+        }
+
+
          [HttpPatch("ActualizarEstadoHabitacion")]
         public async Task<IActionResult> Put(EstadoHabitacion estadoHabitacion)
         {
