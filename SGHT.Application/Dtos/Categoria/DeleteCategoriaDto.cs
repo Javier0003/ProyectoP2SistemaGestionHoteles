dc69d07a43074856be676a6b9dc78fc01469ce49
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-03-01 05:42:02 +0100

    categoria-update

---
+++ SGHT.Application/Dtos/Categoria/DeleteCategoriaDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGHT.Application.Dtos.Categoria
+{
+    public class DeleteCategoriaDto
+    {
+        public int IdCategoria { get; set; }
+    }
+}
---
+++ SGHT.Application/Interfaces/ICategoriaService.cs
@@ -0,0 +1,10 @@
+﻿using SGHT.Application.Base;
+using SGHT.Application.Dtos.Categoria;
+
+namespace SGHT.Application.Interfaces
+{
+    public interface ICategoriaService : IBaseService<SaveCategoriaDto, UpdateCategoriaDto, DeleteCategoriaDto>
+    {
+
+    }
+}
