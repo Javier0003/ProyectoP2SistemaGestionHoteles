ff18a06eb5e8993f3507576b6422943676a1d583
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-03-01 05:42:02 +0100

    categoria-update

---
+++ SGHT.Application/Dtos/Categoria/UpdateCategoriaDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGHT.Application.Dtos.Categoria
+{
+    public class UpdateCategoriaDto : CategoriaDto
+    {
+        public int IdCategoria { get; set; }
+    }
+}
