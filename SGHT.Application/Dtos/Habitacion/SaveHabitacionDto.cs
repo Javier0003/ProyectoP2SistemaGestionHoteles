8293fccb3d4a610840869cbfdd468c2739c77aeb
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-03-02 06:17:05 +0100

    update

--- SGHT.Application/Dtos/Habitacion/SaveHabitacionDto.cs
+++ SGHT.Application/Dtos/Habitacion/SaveHabitacionDto.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace SGHT.Application.Dtos.Habitacion
 {
-<<<<<<< HEAD
     public class SaveHabitacionDto : HabitacionDto
-=======
-    public class SaveHabitacionDto
->>>>>>> 5afcdb74881b35982bc35e15ef6cecc1c460bb41
-    {
+    { 
+
     }
 }
