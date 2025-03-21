fab80c05f9009392b1cbf8e6ece85d4701862948
Jonaifry Rodriguez <jonaifryrodriguezdejesus5@gmail.com>
2025-03-21 02:48:20 -0400

    TestSPersistencia

--- SGHT.Persistance/Repositories/ClienteRepository.cs
+++ SGHT.Persistance/Repositories/ClienteRepository.cs
@@ -26,48 +26,50 @@ namespace SGHT.Persistance.Repositories
             _configuration = configuration;
         }
 
-        //public async Task<OperationResult> GetClienteByID(int idCliente)
-        //{
-        //    OperationResult result = new OperationResult();
+        public async Task<OperationResult> GetClienteByID(int idCliente)
+        {
+            OperationResult result = new OperationResult();
 
-        //    try
-        //    {
-        //        var querys = await (from cliente in _context.Clientes
-        //                            where cliente.IdCliente == idCliente
-        //                            select new Cliente()
-        //                            {
-        //                                IdCliente = cliente.IdCliente,
-        //                                TipoDocumento = cliente.TipoDocumento,
-        //                                Documento = cliente.Documento,
-        //                                NombreCompleto = cliente.NombreCompleto,
-        //                                Correo = cliente.Correo,
-        //                                Estado = cliente.Estado,
-        //                            }).ToListAsync();
-        //        result.Data = querys;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        result.Message = this._configuration["ErroClienteRepository:GetClienteByID"];
-        //        result.Success = false;
-        //        this._logger.LogError(result.Message, ex.ToString());
-        //    }
+            try
+            {
+                var querys = await (from cliente in _context.Clientes
+                                    where cliente.IdCliente == idCliente
+                                    select new Cliente()
+                                    {
+                                        IdCliente = cliente.IdCliente,
+                                        TipoDocumento = cliente.TipoDocumento,
+                                        Documento = cliente.Documento,
+                                        NombreCompleto = cliente.NombreCompleto,
+                                        Correo = cliente.Correo,
+                                        Estado = cliente.Estado,
+                                    }).ToListAsync();
+                result.Data = querys;
+            }
+            catch (Exception ex)
+            {
+                result.Message = this._configuration["ErroClienteRepository:GetClienteByID"];
+                result.Success = false;
+                this._logger.LogError(result.Message, ex.ToString());
+            }
 
-        //    if (result.Data == null) 
-        //        return OperationResult.GetErrorResult("No se encontro el cliente", code: 404);
-               
-       
-        //    return result;
-        //}
+            if (result.Data == null)
+                return OperationResult.GetErrorResult("No se encontro el cliente", code: 404);
+
+
+            return result;
+        }
 
         public override async Task<OperationResult> SaveEntityAsync(Cliente cliente)
         {
-            if(cliente == null) 
-                return OperationResult.GetErrorResult("El cliente es nulo", code: 400);
-            
-            if(cliente.Estado == null) 
-                return OperationResult.GetErrorResult("El estado del cliente no puede ser nulo", code: 400);
-            
+            if(cliente == null) return OperationResult.GetErrorResult("El cliente es nulo", code: 400);
+            if(cliente.Estado == null) return OperationResult.GetErrorResult("El estado del cliente no puede ser nulo", code: 400);
+            if (string.IsNullOrWhiteSpace(cliente.Correo)) return OperationResult.GetErrorResult("Correo can't be null or whitespace", code: 400);
+            if (string.IsNullOrWhiteSpace(cliente.NombreCompleto)) return OperationResult.GetErrorResult("Nombre can't be null or whitespace", code: 400);
+
+
+            if (_context.Clientes.Any(cd => cd.Correo == cliente.Correo)) return OperationResult.GetErrorResult("Este correo ya esta registrado en la base de datos", code: 400);
             return await base.SaveEntityAsync(cliente);
+
         }
 
         public override async Task<OperationResult> UpdateEntityAsync(Cliente cliente)
