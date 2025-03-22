d63bdabd1961640ea24db8824cb315ecfcca194a
Javier Perez <125394473+Javier0003@users.noreply.github.com>
2025-03-22 02:49:44 -0400

    Revert "ValidacionesCambios"

--- SGHT.Domain/Base/OperationResult.cs
+++ SGHT.Domain/Base/OperationResult.cs
@@ -34,15 +34,5 @@ namespace SGHT.Domain.Base
                 Code = code
             };
         }
-
-        //public OperationResult ThenValidate(this OperationResult result, Func<OperationResult> validate)
-        //{
-        //    if (!result.Success)
-        //        return result;
-
-        //    return validate();
-        //}
-
-
     }
 }
