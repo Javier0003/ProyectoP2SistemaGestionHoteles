78ca68900958ea75ca766dfab739398941360f57
Jonaifry Rodriguez <jonaifryrodriguezdejesus5@gmail.com>
2025-03-29 15:26:06 -0400

    a

---
+++ SGHT.Model/Model/Recepcion/ActualizarRecepcionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGHT.Model.Model.Recepcion
+{
+    internal class ActualizarRecepcionModel
+    {
+    }
+}
---
+++ SGHT.Web.Api/Controllers/RecepcionController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SGHT.Web.Api.Controllers
+{
+    public class RecepcionController : Controller
+    {
+    }
+}
