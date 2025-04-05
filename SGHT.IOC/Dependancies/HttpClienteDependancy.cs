607f26c13b30abd15c6847a7a7dee7aa5a1a1c55
Jonaifry Rodriguez <jonaifryrodriguezdejesus5@gmail.com>
2025-04-05 19:09:53 -0400

    RefactorizacionConsumoAPI

---
+++ SGHT.IOC/Dependancies/HttpClienteDependancy.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SGHT.Http.Repositories.Interfaces;
+using SGHT.Http.Repositories.Repositories;
+
+namespace SGHT.IOC.Dependancies
+{
+    public static class HttpClienteDependancy
+    {
+        public static void AddHttpClienteDependancy(this IServiceCollection services)
+        {
+            services.AddTransient<IClienteHttpRepository, ClienteHttpRepository>();
+        }
+    }
+}
--- SGHT.Web.Api/Controllers/RecepcionController.cs
+++ SGHT.Web.Api/Controllers/RecepcionController.cs
@@ -1,49 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SGHT.Domain.Base;
+using SGHT.Web.Api.Base;
 using SGHT.Model.Model.Recepcion;
+using SGHT.Http.Repositories.Interfaces;
 
 namespace SGHT.Web.Api.Controllers
 {
-    public class RecepcionController : Controller
+    public class RecepcionController : BaseController
     {
+
+        private readonly ILogger<RecepcionController> _logger;
+        private readonly IRecepcionHttpRepository _recepcionHttpRepository;
+
+        public RecepcionController(ILogger<RecepcionController> logger, IRecepcionHttpRepository recepcionHttpRepository)
+        {
+            _recepcionHttpRepository = recepcionHttpRepository;
+            _logger = logger;
+        }
+
+
         // GET: RecepcionController
         public async  Task<IActionResult> Index()
         {
-            List<GetRecepcionModel> recepciones = new List<GetRecepcionModel>();
-            using (var client = new HttpClient())
+            try
             {
-                client.BaseAddress = new Uri("http://localhost:5118/api/");
-
-                var response = await client.GetAsync("Recepcion");
-
-                if (response.IsSuccessStatusCode)
-                    recepciones = await response.Content.ReadFromJsonAsync<List<GetRecepcionModel>>();
-                else
-                {
-                    ViewBag.Message = "Error obteniendo las recepciones.";
-                    return View();
-                }
+                var res = await _recepcionHttpRepository.SendGetRequestAsync("Recepcion");
+                return View(await res.Content.ReadFromJsonAsync<List<GetRecepcionModel>>());
             }
-
-            return View(recepciones);
+            catch (Exception ex)
+            {
+                _logger.LogError($"RecepcionController.Index: {ex}");
+                return ViewBagError("Error obteniendo las recepciones.");
+            }
         } 
         
         // GET: RecepcionController/Details/5
         public async Task<IActionResult> Details(int id)
         {
-            using (var client = new HttpClient())
+            try
             {
-                client.BaseAddress = new Uri("http://localhost:5118/api/");
-
-                var response = await client.GetAsync($"Recepcion/{id}");
-
-                if (response.IsSuccessStatusCode)
-                    return View(await response.Content.ReadFromJsonAsync<GetRecepcionModel>());
-                else
-                {
-                    ViewBag.Message = "Error obteniendo la recepcion.";
-                    return View();
-                }
+                var res = await _recepcionHttpRepository.SendGetRequestAsync($"Recepcion/{id}");
+                return View(await res.Content.ReadFromJsonAsync<GetRecepcionModel>());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"RecepcionController.Details: {ex}");
+                return ViewBagError("Error obteniendo la recepcion.");
             }
         }
 
@@ -58,78 +59,67 @@ namespace SGHT.Web.Api.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(CrearRecepcionModel recepcion)
         {
-            using (var client = new HttpClient())
+            try
             {
-                client.BaseAddress = new Uri("http://localhost:5118/api/");
+                if (recepcion == null) throw new ArgumentNullException();
 
-                var response = await client.PostAsJsonAsync("Recepcion", recepcion);
-
-                if (response.IsSuccessStatusCode)
-                    return RedirectToAction("Index");
-                else
-                {
-                    ViewBag.Message = "Error creando la recepcion.";
-                    return View();
-                }
+                var res = await _recepcionHttpRepository.SendPostRequestAsync("Recepcion/crear", recepcion);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"RecepcionController.Create: {ex}");
+                return ViewBagError("Error guardando la recepcion");
             }
         }
 
         // GET: RecepcionController/Edit/5
         public async Task<IActionResult> Edit(int id)
         {
-            using (var client = new HttpClient())
+            try
             {
-                client.BaseAddress = new Uri("http://localhost:5118/api/");
-
-                var response = await client.GetAsync($"Recepcion/{id}");
-
-                if (response.IsSuccessStatusCode)
-                    return View(await response.Content.ReadFromJsonAsync<GetRecepcionModel>());
-                else
-                {
-                    ViewBag.Message = "Error obteniendo la recepcion.";
-                    return View();
-                }
+                var res = await _recepcionHttpRepository.SendGetRequestAsync($"Recepcion/{id}");
+                return View(await res.Content.ReadFromJsonAsync<GetRecepcionModel>());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"RecepcionController.Edit: {ex}");
+                return ViewBagError("Error Obteniendo la recepcion");
             }
         }
 
         // PUT: RecepcionController/Edit/5
-        [HttpPut]
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, ActualizarRecepcionModel recepcion)
         {
-            using (var client = new HttpClient())
+            try
             {
-                client.BaseAddress = new Uri("http://localhost:5118/api/");
+                if (recepcion == null) throw new ArgumentNullException();
 
-                var response = await client.PutAsJsonAsync($"Recepcion/{id}", recepcion);
+                var res = await _recepcionHttpRepository.SendPostRequestAsync("Recepcion/actualizar", recepcion);
+                return RedirectToAction(nameof(Index));
 
-                if (response.IsSuccessStatusCode)
-                    return RedirectToAction("Index");
-                else
-                {
-                    ViewBag.Message = "Error actualizando la recepcion.";
-                    return View();
-                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"clienteController.Edit: {ex}");
+                return RedirectToAction(nameof(Edit));
             }
         }
 
         // GET: RecepcionController/Delete/5
         public async Task<IActionResult> Delete(int id)
         {
-            using (var client = new HttpClient())
+            try
             {
-                client.BaseAddress = new Uri("http://localhost:5118/api/");
-
-                var response = await client.GetAsync($"Recepcion/{id}");
-
-                if (response.IsSuccessStatusCode)
-                    return View(await response.Content.ReadFromJsonAsync<GetRecepcionModel>());
-                else
-                {
-                    ViewBag.Message = "Error obteniendo la recepcion.";
-                    return View();
-                }
+                var res = await _recepcionHttpRepository.SendGetRequestAsync($"Recepcion/{id}");
+                return View(await res.Content.ReadFromJsonAsync<GetRecepcionModel>());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"RecepcionController.Delete: {ex}");
+                return ViewBagError("Error eliminando la recepcion");
             }
         }
 
@@ -140,30 +130,15 @@ namespace SGHT.Web.Api.Controllers
         {
             try
             {
-                using (var client = new HttpClient())
-                {
-                    client.BaseAddress = new Uri("http://localhost:5118/api/");
+                if (recepcionModel == null) throw new ArgumentNullException();
 
-                    var request = new HttpRequestMessage(HttpMethod.Delete, "Recepcion/eliminar")
-                    {
-                        Content = JsonContent.Create(recepcionModel)
-                    };
-
-                    var response = await client.SendAsync(request);
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        return RedirectToAction(nameof(Index));
-                    }
-
-                    ViewBag.Message = "Error eliminando la recepcion.";
-                    return View();
-                }
-
+                var res = await _recepcionHttpRepository.SendDeleteRequestAsync("Recepcion/eliminar", recepcionModel);
+                return RedirectToAction(nameof(Index));
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                return View();
+                _logger.LogError($"RecepcionController.Delete: {ex}");
+                return ViewBagError("Error eliminando a la recepcion");
             }
         }
 
